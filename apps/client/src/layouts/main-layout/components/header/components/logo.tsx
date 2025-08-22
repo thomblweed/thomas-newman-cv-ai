04934060c4573a9c62cd5693ba240211f88c577e
thom <thomblweed@gmail.com>
2025-08-22 13:06:19 +0100

    style updates for responsive fonts

--- apps/client/src/layouts/main-layout/components/header/components/logo.tsx
+++ apps/client/src/layouts/main-layout/components/header/components/logo.tsx
@@ -3,8 +3,8 @@ export const Logo = () => (
     <div className="text-primary text-[2.125rem] md:text-[2.9rem] leading-[1]">
       thomas newman
     </div>
-    <div className="text-secondary text-2xl md:text-[1.5rem] leading-[1.2] pt-0.5">
-      <span className="before:content-['>_']">frontend developer</span>
+    <div className="text-secondary text-base md:text-2xl leading-[1.2] pt-0.5">
+      <span className="before:content-['>_']">senior frontend engineer</span>
     </div>
   </div>
 );
